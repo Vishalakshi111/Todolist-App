1b5b0a37366b7e079d99e80e81db00a9aab2bced
HP <HP@VISHU>
2026-07-25 12:14:50 +0530

    Add project files.

---
+++ Todolist App/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Todolist_App.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
---
+++ Todolist App/Models/TaskItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todolist_App.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public bool IsDone { get; set; }
+    }
+}
